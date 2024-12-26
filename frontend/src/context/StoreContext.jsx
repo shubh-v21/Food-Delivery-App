4adc10980c0de96efde139803fa7c7b2552984d7
Shubh Verma <shubhverma2003@gmail.com>
2024-12-26 20:58:49 +0530

    Implemented cart functionality with item removal, total calculation, and promo code input

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,26 +4,32 @@ import { food_list } from "../assets/assets";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
 
-    const [cartItems , setCartItems] = useState({});
-
-    const addToCart = (itemId) => {
-        if (!cartItems[itemId]) {
-            setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
-        }
-        else{
-            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] +1}))
-        }
+  const addToCart = (itemId) => {
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
+  };
 
-    const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({...prev , [itemId]:prev[itemId] -1 }))
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = ()=>{
+    let totalAmount = 0 ;
+    for(const item in cartItems)
+    {
+      if(cartItems[item]>0){
+        let itemInfo = food_list.find((product)=>product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
+     
     }
-
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems])
-
+    return totalAmount;
+  }
 
   const contextValue = {
     food_list,
@@ -31,6 +37,7 @@ const StoreContextProvider = (props) => {
     setCartItems,
     addToCart,
     removeFromCart,
+    getTotalCartAmount
   };
 
   return (
