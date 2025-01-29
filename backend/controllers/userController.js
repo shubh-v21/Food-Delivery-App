4075c5ab67cbc0f268e097c0ef4b0d0803f18d23
Shubh Verma <shubhverma2003@gmail.com>
2025-01-29 11:34:52 +0530

    user login functionality added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,26 @@ import validator from "validator";
 
 //Login User
 
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.json({ success: false, message: "Invalid password" });
+    }
+    const token = createToken(user._id);
+    res.json({ success: true, message: "Logged in successfully", token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
 
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET);
