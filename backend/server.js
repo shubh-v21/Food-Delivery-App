2a26dbc8e2f6b8b9aaebb8dc30535db3d9eba2a3
Shubh Verma <shubhverma2003@gmail.com>
2025-01-29 11:17:28 +0530

    user register functionality added

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import 'dotenv/config'
 
 // app config
-const app = express()
+const app = express();
 
-const port = 4000
+const port = 4000;
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //db connection
 
@@ -18,16 +20,16 @@ connectDB();
 
 //API endpoints
 
-app.use("/api/food" , foodRouter)
-app.use("/images" , express.static('uploads'))
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/user" , userRouter)
 
-app.get("/" , (req,res)=>{
-res.send("API Working")
-})
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-app.listen(port,()=>{
-    console.log(`Server started on http://localhost:${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
 
 //mongodb+srv://shubhverma2003:12345Bolo&@cluster0.vfvn1.mongodb.net/?
