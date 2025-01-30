ad2a680c870c049a0f8c1d923a28f27cc73d77bc
Shubh Verma <148188973+shubh-v21@users.noreply.github.com>
2025-01-30 15:30:57 +0530

    Update frontend URL in orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // PLacing user orders from frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://food-del-frontend-dlfy.onrender.com";
 
   try {
     const newOrder = new orderModel({
